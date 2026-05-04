f63a981244f904c4c3a7733a44049c6644c23627
andikazafka <andikazafka04@gmail.com>
2026-05-04 20:06:33 +0700

    feat: priority audit done

---
+++ Migrations/20260504130408_AddAuditPrioriy.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QualiTrack.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuditPrioriy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "AuditPlans",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "AuditPlans");
+        }
+    }
+}
